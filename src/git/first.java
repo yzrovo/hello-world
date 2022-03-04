f40026ecd7b5f6d7bca2aba2efc442cbacefff08
yzrovo <3353431272@qq.com>
2022-03-04 21:10:35 +0800

    src/git/first.java

--- src/git/first.java
+++ src/git/first.java
@@ -3,8 +3,13 @@ package git;
 public class first {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		//System.out.println("Welcome to Java!");
+		/*System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");*/
+		/*System.out.println("3.5*4/2-2.5 is ");
+		System.out.println(3.5*4/2-2.5);*/
+		
 	}
 
 }
